67fdb4c22a71dbe937e3447d9fd0c7803f32ca95
cenk <cenk.bilgen@gmail.com>
2025-04-11 10:17:22 -0400

    remove combine publisher

--- Sources/SwiftCamera/SamplePublisher.swift
+++ Sources/SwiftCamera/SamplePublisher.swift
@@ -5,40 +5,40 @@
 //  Created by cenk on 2025-04-03.
 //
 
-@preconcurrency import Combine
-
-extension CameraModel {
-    
-    // NOTE: This function throws, but... if it doesn't throw the publisher it returns does not send any Failure, ie Failure == Never.
-
-    // Basically, two possible failures, 1. Fail on streaming setup (ie no camera session or bad config), 2. Setup does not fail, but camera has errors while streaming (ie dropped frames).
-    // We only catch the first kind, ignore the second.
-    
-    nonisolated public func startCaptureVideoStreamPublisher() throws -> AnyPublisher<CameraModel.SampleBuffer, Never> {
-        let stream = try self.startCaptureVideoStream()
-
-        return Deferred {
-            let subject = PassthroughSubject<CameraModel.SampleBuffer, Never>()
-            let task = Task(priority: .userInitiated) {
-                do {
-                    for await sample in stream {
-                        if Task.isCancelled {
-                            break
-                        }
-                        subject.send(sample)
-                    }
-                } catch {
-                    
-                }
-                subject.send(completion: .finished)
-            }
-
-            return subject
-                .handleEvents(receiveCancel: {
-                    task.cancel()
-                })
-        }
-        .eraseToAnyPublisher()
-    }
-    
-}
+//@preconcurrency import Combine
+//
+//extension CameraModel {
+//    
+//    // NOTE: This function throws, but... if it doesn't throw the publisher it returns does not send any Failure, ie Failure == Never.
+//
+//    // Basically, two possible failures, 1. Fail on streaming setup (ie no camera session or bad config), 2. Setup does not fail, but camera has errors while streaming (ie dropped frames).
+//    // We only catch the first kind, ignore the second.
+//    
+//    nonisolated public func startCaptureVideoStreamPublisher() throws -> AnyPublisher<CameraModel.SampleBuffer, Never> {
+//        let stream = try self.startCaptureVideoStream()
+//
+//        return Deferred {
+//            let subject = PassthroughSubject<CameraModel.SampleBuffer, Never>()
+//            let task = Task(priority: .userInitiated) {
+//                do {
+//                    for await sample in stream {
+//                        if Task.isCancelled {
+//                            break
+//                        }
+//                        subject.send(sample)
+//                    }
+//                } catch {
+//                    
+//                }
+//                subject.send(completion: .finished)
+//            }
+//
+//            return subject
+//                .handleEvents(receiveCancel: {
+//                    task.cancel()
+//                })
+//        }
+//        .eraseToAnyPublisher()
+//    }
+//    
+//}
